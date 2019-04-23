5113a4953eadb59bea46663dd96c54a8e6f56993
NorhanMostafa94 <you@example.com>
2019-04-23 12:03:14 +0200

    create error handler and not found middleware

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var createError = require('http-errors')
+
+require('./db');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,5 +20,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//not found middleware
+app.use((req,res,next)=>{
+    next(createError(404));
+})
+
+//error handler
+app.use((err,req,res,next)=>{
+    console.error(err)
+    res.status(err.status||500);
+    res.send(err)
+})
 
 module.exports = app;
