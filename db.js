d72dbb49f55f5a5308a6e7de53528ce66292e70b
DoaaOsama <doaa_kandeel@hotmail.com>
2019-04-29 10:30:08 +0200

    add dbURL

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbURL = "mongodb://localhost:27017/GoodReadsApp";
+const dbURL = process.env.MONGO_URL || "mongodb://localhost:27017/GoodReadsApp";
 // "mongodb+srv://DB-GoodReads:1234@cluster0-92so5.mongodb.net/test?retryWrites=true";
 
 mongoose.connect(dbURL, {
